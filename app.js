e8928d2250af0c337bbcdd5cab68b999c7e2ed1b
Joseph McCarron <joe@zendesk.com>
2016-01-22 14:17:28 -0800

    cleanup, add nav bar, add images

--- app.js
+++ app.js
@@ -1,12 +1,79 @@
+/*global Blob*/
+/*global URL*/
+/*global File*/
+/*global FileReader*/
+/*global XMLHttpRequest*/
+/*global FormData*/
 (function() {
-
   return {
-    events: {
-      'app.activated':'doSomething'
+    defaultState: 'home',
+    requests: {
     },
+    events: {
+      'pane.created': 'onAppCreated',
+      'click .insert_link': 'insertLinkClicked',
+      'click .attach_file': 'attachFile',
+      // ajax requests
+      'getFromBox.done': 'gotFromBox',
 
-    doSomething: function() {
+      // iframe events
+      'iframe.fileSelected': 'onFileSelected'
+    },
+    onAppCreated: function() {
+      var loc = this.currentLocation();
+      console.dir(loc);
+      if(loc == 'nav_bar') {
+        this.switchTo('nav_bar_home', {
+          uri: 'https://app.box.com/embed_widget/files/0/f/0'
+        });
+        var height = this.$('section').height();
+        console.dir(height);
+      }
+    },
+    onFileSelected: function(e, files) {
+      console.log('File Selected and returned to Zendesk app');
+      console.dir(files);
+      files = _.map(files, function(file) {
+        file.folder = file.type == 'folder';
+        return file;
+      });
+      // insert the links
+      // TODO make the below loop conditional on a setting?
+      // if( this.setting("auto") ) {
+      //   
+      // }
+      _.each(files, function(file) {
+        this.insertLink(file);
+      }, this);
+      this.renderFiles(files);
+    },
+    renderFiles: function(files) {
+      console.log('Rendering Files');
+      
+      this.switchTo('files', {
+        files: files
+      });
+    },
+    insertLinkClicked: function(e) {
+      console.dir(e);
+      var file = {
+        url: e.currentTarget.dataset.fileUri,
+        name: e.currentTarget.dataset.fileName
+      };
+      this.insertLink(file);
+    },
+    insertLink: function(file) {
+      console.dir(file);
+      // 
+      // TODO make this conditional on a user setting (localStorage?) or app setting?
+      if( this.comment().useRichText() ) {
+        var htmlString = helpers.fmt('<a href="%@">%@</a><br>', file.url, file.name);
+        this.comment().appendHtml(htmlString);
+      } else {
+        // services.notify('Rich text is not enabled :(');
+          console.log(file.url);
+        this.comment().appendText(file.url);
+      }
     }
   };
-
 }());
